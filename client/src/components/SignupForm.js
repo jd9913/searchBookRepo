e7fec2c03cc3a612dabbedcc92d4b2937f94ff1d
jd9913 <67403842+jd9913@users.noreply.github.com>
2020-12-30 16:01:33 -0700

    connecting back, front ends

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { CardText } from 'react-bootstrap/Card';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
@@ -13,6 +14,8 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+  const [addUser]=useMutation(ADD_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -21,27 +24,18 @@ const SignupForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
+  
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+     const { data }=await addUser({
+       variables: { ...userFormData }
+     });
+     Auth.login(data.addUser.token)
+     
+      } catch(e){
+        console.error(e);
+        setShowAlert(true);
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
 
     setUserFormData({
       username: '',
