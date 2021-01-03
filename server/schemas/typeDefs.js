05487e554c57039eadcb237f9559d22b946e2c9b
Jennifer Schlierman <jd9913@gmail.com>
2021-01-02 20:24:09 -0700

    update typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,15 +23,30 @@ type User{
 }
 
 
+input savedBook{
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
 type Query{
     me: User
     
 }
+
+type Auth{
+    token: ID
+    user: User
+}
+
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: savedBook!): User
-    removeBook(bookId: ID!): User
+    removeBook(bookId: String!): User
 }
 `;
 
